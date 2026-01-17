130eb69ac7f96353f9522b025442f2e49e6d8327
irritop <129836405+irritop@users.noreply.github.com>
2026-01-17 17:44:38 +0200

    Custom Header 1

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -62,6 +62,7 @@ export default defineNuxtConfig({
         'vue-tel-input',
         'vuedraggable/src/vuedraggable',
         'yup',
+        './modules/custom-header',
       ],
     },
     build: {
